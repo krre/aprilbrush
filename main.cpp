c83da98ce2188a1934c4d9be2546a496fb0fee55
Vladimir Zarypov <krre31@gmail.com>
2012-10-07 15:52:48 +0300

    Simple GUI

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
-int main()
+#include <QApplication>
+#include <QMainWindow>
+
+const QString appName = "AprilBrush";
+
+int main(int argc, char *argv[])
 {
-    return 0;
+    QApplication app(argc, argv);
+    QMainWindow *mainWindow = new QMainWindow();
+    mainWindow->setWindowTitle(appName);
+    mainWindow->show();
+
+    return app.exec();
 }
