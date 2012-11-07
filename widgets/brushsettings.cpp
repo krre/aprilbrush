2bd09c166bca7877717c280d05d2b25ad9841a6f
Vladimir Zarypov <krre31@gmail.com>
2012-11-07 16:49:45 +0200

    Brush Settings: refactoring

--- widgets/brushsettings.cpp
+++ widgets/brushsettings.cpp
@@ -6,89 +6,43 @@ BrushSettings::BrushSettings(BrushEngine *brush)
     setWindowFlags(Qt::WindowStaysOnTopHint | Qt::Tool);
     resize(400, -1);
 
-    int resetButtonWidth = 30;
     QGridLayout *gridLayout = new QGridLayout;
 
-    // Size
-    QLabel *sizeLabel = new QLabel(tr("Size"));
-    sizeSlider = new QSlider(Qt::Horizontal);
-    sizeSlider->setRange(sizeValues[minValue], sizeValues[maxValue]);
-    sizeSlider->setValue(sizeValues[defaultValue]);
-    QSpinBox *sizeSpinBox = new QSpinBox();
-    sizeSpinBox->setMaximum(sizeValues[maxValue]);
-    sizeSpinBox->setValue(sizeValues[defaultValue]);
-    QPushButton *sizeButton = new QPushButton(tr("R"));
-    sizeButton->setMaximumWidth(resetButtonWidth);
-    QLabel *sizeMeasure = new QLabel(tr("px"));
-    connect(sizeSlider, SIGNAL(valueChanged(int)), sizeSpinBox, SLOT(setValue(int)));
-    connect(sizeSpinBox, SIGNAL(valueChanged(int)), sizeSlider, SLOT(setValue(int)));
-    connect(sizeSlider, SIGNAL(valueChanged(int)), brush, SLOT(setSizeBrush(int)));
-    connect(sizeButton, SIGNAL(clicked()), this, SLOT(resetSizeSlider()));
-
-    gridLayout->addWidget(sizeLabel, 0, 0);
-    gridLayout->addWidget(sizeSlider, 0, 1);
-    gridLayout->addWidget(sizeSpinBox, 0, 2);
-    gridLayout->addWidget(sizeMeasure, 0, 3);
-    gridLayout->addWidget(sizeButton, 0, 4);
-
-    // Opacity
-    QLabel *opacityLabel = new QLabel(tr("Opacity"));
-    opacitySlider = new QSlider(Qt::Horizontal);
-    opacitySlider->setRange(opacityValues[minValue], opacityValues[maxValue]);
-    opacitySlider->setValue(opacityValues[defaultValue]);
-    QSpinBox *opacitySpinBox = new QSpinBox();
-    opacitySpinBox->setMaximum(opacityValues[maxValue]);
-    opacitySpinBox->setValue(opacityValues[defaultValue]);
-    QPushButton *opacityButton = new QPushButton(tr("R"));
-    opacityButton->setMaximumWidth(resetButtonWidth);
-    QLabel *opacityMeasure = new QLabel(tr("%"));
-    connect(opacitySlider, SIGNAL(valueChanged(int)), opacitySpinBox, SLOT(setValue(int)));
-    connect(opacitySpinBox, SIGNAL(valueChanged(int)), opacitySlider, SLOT(setValue(int)));
-    connect(opacitySlider, SIGNAL(valueChanged(int)), brush, SLOT(setAlpha(int)));
-    connect(opacityButton, SIGNAL(clicked()), this, SLOT(resetOpacitySlider()));
-
-    gridLayout->addWidget(opacityLabel, 1, 0);
-    gridLayout->addWidget(opacitySlider, 1, 1);
-    gridLayout->addWidget(opacitySpinBox, 1, 2);
-    gridLayout->addWidget(opacityMeasure, 1, 3);
-    gridLayout->addWidget(opacityButton, 1, 4);
-
-    // Space
-    QLabel *spaceLabel = new QLabel(tr("Space"));
-    spaceSlider = new QSlider(Qt::Horizontal);
-    spaceSlider->setRange(spaceValues[minValue], spaceValues[maxValue]);
-    spaceSlider->setValue(spaceValues[defaultValue]);
-    QSpinBox *spaceSpinBox = new QSpinBox();
-    spaceSpinBox->setMaximum(spaceValues[maxValue]);
-    spaceSpinBox->setValue(spaceValues[defaultValue]);
-    QPushButton *spaceButton = new QPushButton(tr("R"));
-    spaceButton->setMaximumWidth(resetButtonWidth);
-    QLabel *spaceMeasure = new QLabel(tr("%"));
-    connect(spaceSlider, SIGNAL(valueChanged(int)), spaceSpinBox, SLOT(setValue(int)));
-    connect(spaceSpinBox, SIGNAL(valueChanged(int)), spaceSlider, SLOT(setValue(int)));
-    connect(spaceSlider, SIGNAL(valueChanged(int)), brush, SLOT(setSpaceBrush(int)));
-    connect(spaceButton, SIGNAL(clicked()), this, SLOT(resetSpaceSlider()));
-
-    gridLayout->addWidget(spaceLabel, 2, 0);
-    gridLayout->addWidget(spaceSlider, 2, 1);
-    gridLayout->addWidget(spaceSpinBox, 2, 2);
-    gridLayout->addWidget(spaceMeasure, 2, 3);
-    gridLayout->addWidget(spaceButton, 2, 4);
+    addSlider(brush, gridLayout, tr("Size"), 1, 20, 150, SLOT(setSizeBrush(int)), 0);
+    addSlider(brush, gridLayout, tr("Opacity"), 0, 50, 100, SLOT(setAlpha(int)), 1);
+    addSlider(brush, gridLayout, tr("Space"), 1, 25, 500, SLOT(setSpaceBrush(int)), 3);
 
     setLayout(gridLayout);
 }
 
-void BrushSettings::resetSizeSlider()
+void BrushSettings::resetSlider(QSlider *slider, int defaultValue)
 {
-    sizeSlider->setValue(sizeValues[defaultValue]);
+    slider->setValue(defaultValue);
 }
 
-void BrushSettings::resetOpacitySlider()
+void BrushSettings::addSlider(BrushEngine *brush, QGridLayout *layout, QString name,
+                                 int minValue, int defaultValue, int maxValue, const char *slot, int row)
 {
-    opacitySlider->setValue(opacityValues[defaultValue]);
-}
+    QLabel *label = new QLabel(name);
+    layout->addWidget(label, row, 0);
 
-void BrushSettings::resetSpaceSlider()
-{
-    spaceSlider->setValue(spaceValues[defaultValue]);
+    QSlider *slider = new QSlider(Qt::Horizontal);
+    slider->setRange(minValue, maxValue);
+    slider->setValue(defaultValue);
+    layout->addWidget(slider, row, 1);
+
+    QSpinBox *spinBox = new QSpinBox();
+    spinBox->setMaximum(maxValue);
+    spinBox->setValue(defaultValue);
+    layout->addWidget(spinBox, row, 3);
+
+    QPushButton *button = new QPushButton(tr("R"));
+    button->setMaximumWidth(30);
+    layout->addWidget(button, row, 4);
+
+    connect(slider, SIGNAL(valueChanged(int)), spinBox, SLOT(setValue(int)));
+    connect(slider, SIGNAL(valueChanged(int)), brush, slot);
+    connect(spinBox, SIGNAL(valueChanged(int)), slider, SLOT(setValue(int)));
+
+    //connect(button, SIGNAL(clicked()), this, SLOT(resetSlider()));
 }
--- widgets/brushsettings.h
+++ widgets/brushsettings.h
@@ -4,11 +4,6 @@
 #include <QtGui>
 #include "../brushengine.h"
 
-// nameValues[] = {minValues, defaultValues, maxValues}
-const int sizeValues[] = {1, 20, 100};
-const int opacityValues[] = {0, 50, 100};
-const int spaceValues[] = {1, 25, 500};
-
 class BrushSettings : public QWidget
 {
     Q_OBJECT
@@ -17,15 +12,11 @@ public:
     BrushSettings(BrushEngine *brush);
 
 private:
-    enum {minValue, defaultValue, maxValue};
-    QSlider *sizeSlider;
-    QSlider *opacitySlider;
-    QSlider *spaceSlider;
+    void addSlider(BrushEngine *brush, QGridLayout *layout, QString name,
+                   int minValue, int defaultValue, int maxValue, const char *slot, int row);
 
 private slots:
-    void resetSizeSlider();
-    void resetOpacitySlider();
-    void resetSpaceSlider();
+    void resetSlider(QSlider *slider, int defaultValue);
 };
 
 #endif // BRUSHSETTINGS_H
