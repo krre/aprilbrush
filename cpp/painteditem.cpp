17fe956fa938ca1bf95618fba8d62cc0e1f86af6
Vladimir Zarypov <krre31@gmail.com>
2013-04-18 16:30:59 +0300

    Disabled render target FramebufferObject for painted item
    This setting creates new layers with unwanted black fill color

--- cpp/painteditem.cpp
+++ cpp/painteditem.cpp
@@ -5,7 +5,7 @@
 PaintedItem::PaintedItem(QQuickItem *parent) : QQuickPaintedItem(parent)
 {
     pixmap = 0;
-    setRenderTarget(QQuickPaintedItem::FramebufferObject);
+    //setRenderTarget(QQuickPaintedItem::FramebufferObject);
 }
 
 void PaintedItem::paint(QPainter *painter)
