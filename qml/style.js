5cecc8a3177b25ad4486516e24fd744c06d07ed3
Vladimir Zarypov <krre31@gmail.com>
2014-09-11 17:14:50 +0300

    removed unused file style.js

--- qml/style.js
+++
@@ -1,9 +0,0 @@
-function defaultStyle() {
-    return {
-        appBgColor: "#262626",
-        toolBgColor: "#4f4f4f",
-        toolHeaderColor1: "#545454",
-        toolHeaderColor2: "454545",
-        borderColor: "#333333"
-    }
-}
