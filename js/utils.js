701a552383824b9ff2f577cdfd5d7714d3df4488
Vladimir Zarypov <krre31@gmail.com>
2015-12-11 06:47:05 +0200

    refactored createDynamicObject() function in utils.js

--- js/utils.js
+++ js/utils.js
@@ -204,7 +204,7 @@ function createDynamicObject(parent, url, properties) {
     var component = Qt.createComponent(url)
     var errorMessage = component.errorString()
     if (errorMessage) {
-        print("Error loading component " + url + ":", errorMessage)
+        print(errorMessage)
     } else {
         return component.createObject(parent, properties || {})
     }
