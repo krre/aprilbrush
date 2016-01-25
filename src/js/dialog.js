1b21c1715f422a3931c426bc05622d611e6ec1fc
Vladimir Zarypov <krre31@gmail.com>
2016-01-25 08:46:42 +0200

    replaced full url strings by variable in dialog.js

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -1,23 +1,25 @@
 .import QtQuick.Dialogs 1.2 as Dialogs
 .import "../js/utils.js" as Utils
 
+var messageDialogUrl = "qrc:/qml/components/MessageDialogBase.qml"
+
 function info(message, parent) {
-    return Utils.createDynamicObject(parent || mainRoot, "qrc:/qml/components/MessageDialogBase.qml",
+    return Utils.createDynamicObject(parent || mainRoot, messageDialogUrl,
                                      { icon: Dialogs.StandardIcon.Information, text: message })
 }
 
 function warning(message, parent) {
-    return Utils.createDynamicObject(parent || mainRoot, "qrc:/qml/components/MessageDialogBase.qml",
+    return Utils.createDynamicObject(parent || mainRoot, messageDialogUrl,
                                      { icon: Dialogs.StandardIcon.Information, text: message })
 }
 
 function question(message, parent) {
-    return Utils.createDynamicObject(parent || mainRoot, "qrc:/qml/components/MessageDialogBase.qml",
+    return Utils.createDynamicObject(parent || mainRoot, messageDialogUrl,
                                      { icon: Dialogs.StandardIcon.Information, standardButtons: Dialogs.StandardButton.Yes | Dialogs.StandardButton.No, text: message })
 }
 
 function error(message, parent) {
-    return Utils.createDynamicObject(parent || mainRoot, "qrc:/qml/components/MessageDialogBase.qml",
+    return Utils.createDynamicObject(parent || mainRoot, messageDialogUrl,
                                      { icon: Dialogs.StandardIcon.Critical, text: message })
 }
 
