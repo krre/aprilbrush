61a40397bb60cb26f9dbe63a654666a08d48f469
Vladimir Zarypov <krre31@gmail.com>
2019-11-30 16:07:15 +0200

    Add saving and restoring window geometry

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -1,5 +1,30 @@
 #include "MainWindow.h"
+#include <QtWidgets>
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
+    readSettings();
+}
 
+void MainWindow::closeEvent(QCloseEvent* event) {
+    writeSettings();
+    event->accept();
+}
+
+void MainWindow::readSettings() {
+    QSettings settings;
+
+    QByteArray geometry = settings.value("geometry", QByteArray()).toByteArray();
+
+    if (geometry.isEmpty()) {
+        QRect availableGeometry = QApplication::desktop()->availableGeometry(this);
+        resize(availableGeometry.width() * 2 / 3, availableGeometry.height() * 2 / 3);
+        move((availableGeometry.width() - width()) / 2, (availableGeometry.height() - height()) / 2);
+    } else {
+        restoreGeometry(geometry);
+    }
+}
+
+void MainWindow::writeSettings() {
+    QSettings settings;
+    settings.setValue("geometry", saveGeometry());
 }
--- src/ui/MainWindow.h
+++ src/ui/MainWindow.h
@@ -5,4 +5,11 @@ class MainWindow : public QMainWindow {
     Q_OBJECT
 public:
     explicit MainWindow(QWidget* parent = nullptr);
+
+protected:
+    void closeEvent(QCloseEvent* event) override;
+
+private:
+    void readSettings();
+    void writeSettings();
 };
