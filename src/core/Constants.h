79143a5e84751018a5cff9ac6656c883aed053f4
Vladimir Zarypov <krre31@gmail.com>
2019-11-30 15:35:45 +0200

    Change indent in Constants.h

--- src/core/Constants.h
+++ src/core/Constants.h
@@ -1,10 +1,11 @@
 #pragma once
 
 namespace Const {
-    namespace App  {
-        constexpr auto Name = "AprilBrush";
-        constexpr auto Version = "0.3.0";
-        constexpr auto URL = "https://github.com/krre/aprilbrush";
-        constexpr auto CopyrightYear = "2012-2019";
-    }
-}
+namespace App  {
+    constexpr auto Name = "AprilBrush";
+    constexpr auto Version = "0.3.0";
+    constexpr auto URL = "https://github.com/krre/aprilbrush";
+    constexpr auto CopyrightYear = "2012-2019";
+} // App
+
+} // Const
