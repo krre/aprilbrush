45fa0787576e1fbc0c8b9de7a4fc739ce05f1af9
Vladimir Zarypov <krre31@gmail.com>
2019-12-01 08:24:29 +0200

    Select new tab after creating

--- src/ui/CanvasTabWidget.cpp
+++ src/ui/CanvasTabWidget.cpp
@@ -13,6 +13,7 @@ void CanvasTabWidget::addCanvas(const QSize& size) {
     QString tabName = tr("Untitled-%1").arg(m_maxTabCount++);
     QSize canvasSize = size.isEmpty() ? Utils::defaultCanvasSize() : size;
     addTab(new Canvas(canvasSize), tabName);
+    setCurrentIndex(count() - 1);
 }
 
 void CanvasTabWidget::closeTab(int index) {
