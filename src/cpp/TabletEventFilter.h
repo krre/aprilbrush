2653a897bd1bcb4a87e92132671152ea6f483299
Vladimir Zarypov <krre31@gmail.com>
2021-11-01 07:54:44 +0200

    Cleanup code

--- src/cpp/TabletEventFilter.h
+++ src/cpp/TabletEventFilter.h
@@ -4,11 +4,8 @@
 
 class TabletEventFilter : public QObject {
     Q_OBJECT
-
 signals:
     void action(QVariantMap event);
-
-public slots:
 
 protected:
     bool eventFilter(QObject* obj, QEvent* event);
