cf44f7d9f6ce99428aad0085c68c6f29d19ebb88
Vladimir Zarypov <krre31@gmail.com>
2021-11-01 08:09:12 +0200

    Remove switching to QML

--- src/main.cpp
+++ src/main.cpp
@@ -22,59 +22,27 @@ int main(int argc, char* argv[]) {
     QSettings::setDefaultFormat(QSettings::IniFormat);
 #endif
 
-    QCommandLineParser parser;
+    QSettings settings;
+    QString language = settings.value("language").toString();
 
-    QCommandLineOption javaScriptOption("j");
-    parser.addOption(javaScriptOption);
-    parser.process(app);
-
-    if (parser.isSet(javaScriptOption)) {
-        qmlRegisterType<CanvasItem>("AprilBrush", 1, 0, "CanvasItem");
-
-        TabletEventFilter tabletEventFilter;
-        app.installEventFilter(&tabletEventFilter);
-
-        QString filePath = QApplication::applicationDirPath() + "/aprilbrush.ini";
-        Settings settings(filePath);
-
-        BrushEngine brushEngine;
-        Core core;
-
-        QQmlApplicationEngine engine;
-
-        engine.rootContext()->setContextProperty("TabletEventFilter", &tabletEventFilter);
-        engine.rootContext()->setContextProperty("Settings", &settings);
-        engine.rootContext()->setContextProperty("BrushEngine", &brushEngine);
-        engine.rootContext()->setContextProperty("Core", &core);
-        engine.load(QUrl("qrc:/qml/Main.qml"));
-
-        if (engine.rootObjects().isEmpty()) return EXIT_FAILURE;
-
-        core.setMainWindow(qobject_cast<QQuickWindow*>(engine.rootObjects().at(0)));
-        return QApplication::exec();
-    } else {
-        QSettings settings;
-        QString language = settings.value("language").toString();
-
-        if (language.isEmpty()) {
-            language = QLocale::system().name().split("_").first();
-        }
-
-        auto qtTranslator = new QTranslator(&app);
-
-        if (qtTranslator->load("qt_" + language, QLibraryInfo::path(QLibraryInfo::TranslationsPath))) {
-            QCoreApplication::installTranslator(qtTranslator);
-        }
-
-        auto appTranslator = new QTranslator(&app);
-
-        if (appTranslator->load("app-" + language, ":/i18n")) {
-            QCoreApplication::installTranslator(appTranslator);
-        }
-
-        MainWindow mainWindow;
-        mainWindow.show();
-
-        return QApplication::exec();
+    if (language.isEmpty()) {
+        language = QLocale::system().name().split("_").first();
     }
+
+    auto qtTranslator = new QTranslator(&app);
+
+    if (qtTranslator->load("qt_" + language, QLibraryInfo::path(QLibraryInfo::TranslationsPath))) {
+        QCoreApplication::installTranslator(qtTranslator);
+    }
+
+    auto appTranslator = new QTranslator(&app);
+
+    if (appTranslator->load("app-" + language, ":/i18n")) {
+        QCoreApplication::installTranslator(appTranslator);
+    }
+
+    MainWindow mainWindow;
+    mainWindow.show();
+
+    return QApplication::exec();
 }
