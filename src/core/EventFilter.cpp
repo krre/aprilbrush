1a7a3b5f99cfe2ba1b474a2b7a2b804a03f4c534
Vladimir Zarypov <krre31@gmail.com>
2023-07-29 09:39:59 +0300

    Fix event filter

--- src/core/EventFilter.cpp
+++ src/core/EventFilter.cpp
@@ -9,12 +9,10 @@ bool EventFilter::eventFilter(QObject* watched, QEvent* event) {
     if (event->type() == QEvent::KeyPress) {
         QKeyEvent* keyEvent = static_cast<QKeyEvent*>(event);
         emit keyPressed(keyEvent);
-        return true;
     } else if (event->type() == QEvent::KeyRelease) {
         QKeyEvent* keyEvent = static_cast<QKeyEvent*>(event);
         emit keyReleased(keyEvent);
-        return true;
-    } else {
-        return QObject::eventFilter(watched, event);
     }
+
+    return QObject::eventFilter(watched, event);
 }
