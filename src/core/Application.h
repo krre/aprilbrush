9dbd4dafe23a262c39edb5475731bd6fe20d48f9
Vladimir Zarypov <krre31@gmail.com>
2024-01-07 17:07:29 +0200

    Use project version from CMakeLists.txt

--- src/core/Application.h
+++ src/core/Application.h
@@ -1,10 +1,11 @@
 #pragma once
+#include "config.h"
 #include <QApplication>
 
 class Application : public QApplication {
 public:
     static constexpr auto Name = "AprilBrush";
-    static constexpr auto Version = "0.3.0";
+    static constexpr auto Version = PROJECT_VERSION;
     static constexpr auto Url = "https://github.com/krre/aprilbrush";
     static constexpr auto CopyrightYear = "2012-2024";
     static constexpr auto BuildDate = __DATE__;
