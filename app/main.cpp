c9728b82bbdbdb4810dc8203a301f7ba4a452995
Vladimir Zarypov <krre31@gmail.com>
2024-09-19 06:14:36 +0300

    Set GTK style fixing only for Linux

--- app/main.cpp
+++ app/main.cpp
@@ -2,9 +2,11 @@
 #include <ui/MainWindow.h>
 
 int main(int argc, char* argv[]) {
+#ifdef Q_OS_LINUX
     // Hack to fix styling with Qt 6.5 on GTK3
     qunsetenv("GNOME_DESKTOP_SESSION_ID");
     qunsetenv("XDG_CURRENT_DESKTOP");
+#endif
 
     Application app(argc, argv);
 
