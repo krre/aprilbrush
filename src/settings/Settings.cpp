c1ddc4046608665c4d47dcfedbc741d93f5bf860
Vladimir Zarypov <krre31@gmail.com>
2025-06-22 06:44:55 +0300

    Add constants for settings keys

--- src/settings/Settings.cpp
+++ src/settings/Settings.cpp
@@ -1,49 +1,70 @@
 #include "Settings.h"
 
+namespace Application {
+    constexpr auto Language = "Application/language";
+}
+
+namespace MainWindow {
+    constexpr auto Geometry = "MainWindow/geometry";
+    constexpr auto State = "MainWindow/state";
+}
+
+namespace NewImage {
+    constexpr auto Size = "NewImage/size";
+}
+
+namespace ColorPicker {
+    constexpr auto Color = "Application/color";
+}
+
+namespace InputDevice {
+    constexpr auto Geometry = "Application/geometry";
+}
+
 void Settings::setApplicationLanguage(const QString& lang) {
-    setValue("Application/language", lang);
+    setValue(Application::Language, lang);
 }
 
 QString Settings::applicationLanguage() const {
-    return value("Application/language").toString();
+    return value(Application::Language).toString();
 }
 
 void Settings::setMainWindowGeometry(const QByteArray& geometry) {
-    setValue("MainWindow/geometry", geometry);
+    setValue(MainWindow::Geometry, geometry);
 }
 
 QByteArray Settings::mainWindowGeometry() const {
-    return value("MainWindow/geometry").toByteArray();
+    return value(MainWindow::Geometry).toByteArray();
 }
 
 void Settings::setMainWindowState(const QByteArray& state) {
-    setValue("MainWindow/state", state);
+    setValue(MainWindow::State, state);
 }
 
 QByteArray Settings::mainWindowState() const {
-    return value("MainWindow/state").toByteArray();
+    return value(MainWindow::State).toByteArray();
 }
 
 void Settings::setNewImageSize(const QSize& size) {
-    setValue("NewImage/size", size);
+    setValue(NewImage::Size, size);
 }
 
 QSize Settings::newImageSize() const {
-    return value("NewImage/size").toSize();
+    return value(NewImage::Size).toSize();
 }
 
 void Settings::setColorPickerColor(const QColor& color) {
-    setValue("ColorPicker/color", color);
+    setValue(ColorPicker::Color, color);
 }
 
 QColor Settings::colorPickerColor() const {
-    return value("ColorPicker/color", QColor(Qt::red)).value<QColor>();
+    return value(ColorPicker::Color, QColor(Qt::red)).value<QColor>();
 }
 
 void Settings::setInputDeviceGeometry(const QByteArray& geometry) {
-    setValue("InputDevice/geometry", geometry);
+    setValue(InputDevice::Geometry, geometry);
 }
 
 QByteArray Settings::inputDeviceGeometry() const {
-    return value("InputDevice/geometry").toByteArray();
+    return value(InputDevice::Geometry).toByteArray();
 }
