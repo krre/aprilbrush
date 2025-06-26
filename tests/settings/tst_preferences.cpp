c15d054089bbf15cef17da1c7dc529cdec629be2
Vladimir Zarypov <krre31@gmail.com>
2025-06-26 18:15:38 +0300

    Add `Preferences` dialog tests

--- tests/settings/tst_preferences.cpp
+++ tests/settings/tst_preferences.cpp
@@ -1,5 +1,9 @@
+#include <ui/dialog/Preferences.h>
 #include <settings/Settings.h>
 #include <QTest>
+#include <QComboBox>
+
+constexpr auto Language = "ru";
 
 class TestSettings : public Settings {
 
@@ -25,10 +29,36 @@ private slots:
 
 void TestPreferences::readPreferences() {
     TestSettings settings;
+    settings.setApplicationLanguage(Language);
+
+    Preferences preferences(&settings);
+
+    QTest::keyClick(&preferences, Qt::Key_Tab); // OK button
+    QTest::keyClick(&preferences, Qt::Key_Tab); // Cancel button
+    QTest::keyClick(&preferences, Qt::Key_Tab);
+    auto languageComboBox = static_cast<QComboBox*>(preferences.focusWidget());
+    languageComboBox->setCurrentIndex(languageComboBox->findData(Language));
+
+    QCOMPARE(languageComboBox->currentData(), Language);
 }
 
 void TestPreferences::setPreferences() {
     TestSettings settings;
+
+    // To prevent opening the need restart dialog
+    settings.setApplicationLanguage(Language);
+
+    Preferences preferences(&settings);
+
+    QTest::keyClick(&preferences, Qt::Key_Tab); // OK button
+    QTest::keyClick(&preferences, Qt::Key_Tab); // Cancel button
+    QTest::keyClick(&preferences, Qt::Key_Tab);
+    auto languageComboBox = static_cast<QComboBox*>(preferences.focusWidget());
+    languageComboBox->setCurrentIndex(languageComboBox->findData(Language));
+
+    preferences.accept();
+
+    QCOMPARE(settings.applicationLanguage(), Language);
 }
 
 QTEST_MAIN(TestPreferences)
